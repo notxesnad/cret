b8f689c86842301cb683d3e1fa2df897a1019d5d
notxesnad <notxesnad@gmail.com>
2026-08-06 23:38:40 -0400

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,17 +5,14 @@ export const metadata = {
   description: 'All Micro-App Demos',
 }
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Bungee&family=Inter:wght@400;700;900&family=Playfair+Display:ital,wght@1,900&family=Righteous&family=Syne:wght@800&family=VT323&display=swap" rel="stylesheet" />
-      </head>
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   )
 }
