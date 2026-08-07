3c2d4a741344ba672e7cef72f10487e3baa5a64b
notxesnad <notxesnad@gmail.com>
2026-08-06 23:44:10 -0400

    fixed fonts in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Bungee&family=Inter:wght@400;700;900&family=Playfair+Display:ital,wght@1,900&family=Righteous&family=Syne:wght@800&family=VT323&display=swap" rel="stylesheet" />
+      </head>
       <body>{children}</body>
     </html>
   )
